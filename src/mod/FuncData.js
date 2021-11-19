be12eda7d9a0f4f78a73e89ba24700d91ad96479
MarAngle <yinweiyingzhe@126.com>
2021-11-19 08:59:44 +0800

    依赖数据添加

---
+++ src/mod/FuncData.js
@@ -0,0 +1,166 @@
+import _func from 'complex-func'
+import IdData from './IdData'
+import Data from './../data/Data'
+
+let lifeId = new IdData({
+  list: [
+    {
+      type: 'time'
+    },
+    {
+      type: 'id'
+    }
+  ]
+})
+
+class FuncData extends Data {
+  constructor (initOption = {}) {
+    super()
+    this.data = new Map()
+    if (initOption.data) {
+      this.build(initOption.data)
+    }
+  }
+  /**
+   * 设置生命周期对应函数回调
+   * @param {object} data 参数
+   */
+  pushData(data) {
+    if (data.index === undefined || data.index == 'end') {
+      this.data.set(data.id, data)
+    } else {
+      if (data.index == 'start') {
+        data.index = 0
+      }
+      let size = this.data.size
+      if (data.index < size) {
+        let mapList = []
+        this.data.forEach(function(value) {
+          mapList.push(value)
+        })
+        this.data.clear()
+        for (let n = 0; n < size; n++) {
+          let mapItem = mapList[n]
+          if (data.index == n) {
+            this.data.set(data.id, data)
+          }
+          this.data.set(mapItem.id, mapItem)
+        }
+      } else {
+        this.data.set(data.id, data)
+      }
+    }
+  }
+  /**
+   * build
+   * @param {*} data
+   * @returns {string} id
+   */
+  build(data) {
+    let resId
+    if (data) {
+      let isArray = _func.isArray(data)
+      if (isArray) {
+        resId = []
+        for (let n = 0; n < data.length; n++) {
+          resId.push(this.formatData(data[n]))
+        }
+      } else {
+        resId = this.formatData(data)
+      }
+    }
+    return resId
+  }
+  /**
+   * 格式化数据
+   * @param {object| function} data 回调参数
+   * @returns {boolean}next
+   */
+  formatData(data) {
+    let dataType = _func.getType(data)
+    let next = true
+    if (dataType === 'function') {
+      data = {
+        data: data
+      }
+    } else if (dataType !== 'object') {
+      next = false
+    }
+    if (next) {
+      if (data.data) {
+        if (!data.id) {
+          data.id = lifeId.getData()
+        }
+        if (this.data.has(data.id) && !data.replace) {
+          this.printMsg(`存在当前值:${data.id}`)
+        } else {
+          this.pushData(data)
+          if (data.immediate) {
+            this.emit(data.id)
+          }
+          return data.id
+        }
+      } else {
+        this.printMsg(`设置(${data.id || '-'})未定义func`)
+      }
+    } else {
+      this.printMsg(`设置data参数需要object或者function`)
+    }
+    return undefined
+  }
+  /**
+   * 触发函数
+   * @param  {...any} args 参数
+   */
+  trigger(...args) {
+    for (let id of this.data.keys()) {
+      this.emit(id, ...args)
+    }
+  }
+  /**
+   * 触发指定id的回调
+   * @param {string} id id
+   * @param  {...any} args 参数
+   */
+  emit(id, ...args) {
+    let data = this.data.get(id)
+    if (data && data.data) {
+      data.data(...args)
+      if (data.once) {
+        this.off(id)
+      }
+    } else {
+      this.printMsg(`不存在当前值(${id})`)
+    }
+  }
+  /**
+   * 删除指定id的生命周期
+   * @param {string} id id
+   * @returns {boolean}
+   */
+  off (id) {
+    return this.data.delete(id)
+  }
+  /**
+   * 清空所有回调
+   */
+  clear() {
+    this.data.clear()
+  }
+  /**
+   * 重置
+   */
+  reset () {
+    this.clear()
+  }
+  /**
+   * 销毁
+   */
+  destroy() {
+    this.reset()
+  }
+}
+
+FuncData.$name = 'FuncData'
+
+export default FuncData
---
+++ src/mod/LifeData.js
@@ -0,0 +1,137 @@
+import _func from 'complex-func'
+import Data from './../data/Data'
+import FuncData from './FuncData'
+
+/*
+传参问题不能用apply解决，避免箭头函数产生的this指向错误问题
+生命周期函数暂行方案
+通过Map实现，可实现对应的顺序
+基本周期函数
+created
+beforeLoad
+loaded
+loadFail
+beforeUpdate
+updated
+updateFail
+beforeReset
+reseted
+beforeDestroy
+destroyed
+*/
+
+class LifeData extends Data {
+  constructor (initOption) {
+    super()
+    this.data = {}
+    if (initOption) {
+      this.initData(initOption)
+    }
+  }
+  /**
+   * 加载生命周期状态列表
+   * @param {object} [initOption] 生命周期参数
+   * @param {boolean} [reset = true] 是否重置
+   */
+  initData (initOption = {}, reset = true) {
+    if (reset) {
+      this.reset()
+    }
+    for (let n in initOption) {
+      let item = initOption[n]
+      this.on(n, item)
+    }
+  }
+  /**
+   * 创建对应的生命周期对象:存储
+   * @param {string} name 生命周期名称
+   * @param {boolean} [auto = true] 不存在时自动设置
+   */
+  build(name, auto = true) {
+    if (!this.data[name] && auto) {
+      this.data[name] = new FuncData({
+        name: name
+      })
+    }
+  }
+  /**
+   * 获取对应生命周期对象
+   * @param {string} name 生命周期名称
+   * @param {boolean} [auto = true] 不存在时自动设置
+   * @returns {FuncData}
+   */
+  get(name, auto) {
+    this.build(name, auto)
+    return this.data[name]
+  }
+  /**
+   * 设置生命周期回调
+   * @param {string} name 生命周期名称
+   * @param {*} data FuncData参数
+   * @returns {string | string} id/idList
+   */
+  on (name, data) {
+    let funcItem = this.get(name, true)
+    return funcItem.build(data)
+  }
+  /**
+   * 触发生命周期指定id函数
+   * @param {string} name 生命周期
+   * @param {string} id 指定ID
+   * @param  {...any} args 参数
+   */
+  emit (name, id, ...args) {
+    let funcItem = this.get(name, true)
+    funcItem.emit(id, ...args)
+  }
+  /**
+   * 触发生命周期
+   * @param {string} name 生命周期
+   * @param  {...any} args 参数
+   */
+  trigger (name, ...args) {
+    let funcItem = this.get(name, true)
+    funcItem.trigger(...args)
+  }
+  /**
+   * 删除生命周期指定函数
+   * @param {string} name 生命周期
+   * @param {string} id 指定ID
+   * @returns {boolean}
+   */
+  off (name, id) {
+    let funcItem = this.get(name, false)
+    if (funcItem) {
+      return funcItem.off(id)
+    }
+  }
+  /**
+   * 清除生命周期
+   * @param {string} name 生命周期
+   */
+  clear (name) {
+    let funcItem = this.get(name, false)
+    if (funcItem) {
+      funcItem.clear()
+    }
+  }
+  /**
+   * 重置
+   */
+  reset () {
+    for (let name in this.data) {
+      this.clear(name)
+    }
+  }
+  /**
+   * 销毁
+   */
+  destroy () {
+    this.reset()
+    this.data = {}
+  }
+}
+
+LifeData.$name = 'LifeData'
+
+export default LifeData
