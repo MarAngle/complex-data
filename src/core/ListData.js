14e3a126d9dd9415c1f4f4dd891d6c3084e367d3
MarAngle <yinweiyingzhe@126.com>
2021-12-03 09:04:51 +0800

    考虑添加额外的判断值

--- src/core/ListData.js
+++ src/core/ListData.js
@@ -1,21 +1,13 @@
 import _func from 'complex-func'
-import ComplexData from './../data/ComplexData'
-import PaginationData from './../mod/PaginationData'
-import ChoiceData from './../mod/ChoiceData'
+import BaseData from './../data/BaseData'
 
-class ListData extends ComplexData {
+class ListData extends BaseData {
   constructor (initOption) {
     if (!initOption) {
       initOption = {}
     }
     super(initOption)
     this.triggerCreateLife('ListData', 'beforeCreate', initOption)
-    if (initOption.pagination) {
-      this.setModule('pagination', new PaginationData(initOption.pagination))
-    }
-    if (initOption.choice) {
-      this.setModule('choice', new ChoiceData(initOption.choice))
-    }
     this.$initListDataLife()
     this.triggerCreateLife('ListData', 'created')
   }
@@ -40,42 +32,6 @@ class ListData extends ComplexData {
     })
   }
   /**
-   * 获取分页器数据
-   * @param {'page' | 'size' | 'num'} [prop] 获取'page' | 'size' | 'num'数据或者current{page,size}数据
-   * @returns {number | { page, size }}
-   */
-  getPageData (prop) {
-    let data
-    if (this.$module.pagination) {
-      data = this.$module.pagination.getData(prop)
-    }
-    return data
-  }
-  /**
-   * 重置分页器
-   */
-  resetPageData () {
-    if (this.$module.pagination) {
-      this.$module.pagination.reset()
-    }
-  }
-  /**
-   * 设置分页器数据
-   * @param {number} data 需要设置的属性值
-   * @param {'page' | 'size' | 'num'} [prop = 'page'] 需要设置的参数'page' | 'size' | 'num'
-   */
-  setPageData (data, prop = 'page', unTriggerLife) {
-    if (this.$module.pagination) {
-      if (prop == 'page') {
-        this.$module.pagination.setPage(data, unTriggerLife)
-      } else if (prop == 'size') {
-        this.$module.pagination.setSizeAndPage(data, unTriggerLife) // { size, page }
-      } else if (prop == 'num') {
-        this.$module.pagination.setTotal(data)
-      }
-    }
-  }
-  /**
    * 格式化列表数据
    * @param {object[]} datalist 源数据列表
    * @param {number} [totalnum] 总数
@@ -85,50 +41,6 @@ class ListData extends ComplexData {
   formatData (datalist = [], totalnum, originfromType, option) {
     this.formatListData(this.data.list, datalist, originfromType, option)
     this.setPageData(totalnum, 'num')
-  }
-  /**
-   * 根据option, defaultOption自动判断重置与否
-   * @param {object | string} [option] 参数
-   * @param {object | string} [defaultOption] 默认参数
-   */
-  autoChoiceReset(data) {
-    if (this.$module.choice) {
-      this.$module.choice.autoReset(data)
-    }
-  }
-  /**
-   * 数据变更=>id作为唯一基准
-   * @param {string[]} idList ID列表
-   * @param {object[]} currentList ITEM列表
-   * @param {'auto' | 'force'} [check = 'auto'] 检查判断值,auto在长度相等时直接认为格式符合，否则进行格式化判断
-   * @param {string} [idProp] id的属性,不存在时自动从DL中获取
-   */
-  changeChoice(idList, currentList, check, idProp) {
-    if (this.$module.choice) {
-      if (!idProp) {
-        idProp = this.getDictionaryPropData('prop', 'id')
-      }
-      this.$module.choice.changeData(idList, currentList, check, idProp)
-    }
-  }
-  /**
-   * 重置操作
-   * @param {boolean} force 重置判断值
-   */
-  resetChoice(force) {
-    if (this.$module.choice) {
-      this.$module.choice.reset(force)
-    }
-  }
-  /**
-   * 获取选项数据
-   * @param {'id' | 'list'} [prop] 存在prop获取data[prop]，否则获取{id, list}
-   * @returns {string[] | object[] | {id, list}}
-   */
-  getChoiceData (prop) {
-    if (this.$module.choice) {
-      return this.$module.choice.getData(prop)
-    }
   }
   // --数据相关--*/
   /**
--- src/data/BaseData.js
+++ src/data/BaseData.js
@@ -284,6 +284,92 @@ class BaseData extends DefaultData {
   }
   /* --- dictionary end --- */
 
+  /* --- pagination start --- */
+  /**
+   * 获取分页器数据
+   * @param {'page' | 'size' | 'num'} [prop] 获取'page' | 'size' | 'num'数据或者current{page,size}数据
+   * @returns {number | { page, size }}
+   */
+   getPageData (prop) {
+    let data
+    if (this.$module.pagination) {
+      data = this.$module.pagination.getData(prop)
+    }
+    return data
+  }
+  /**
+   * 重置分页器
+   */
+  resetPageData () {
+    if (this.$module.pagination) {
+      this.$module.pagination.reset()
+    }
+  }
+  /**
+   * 设置分页器数据
+   * @param {number} data 需要设置的属性值
+   * @param {'page' | 'size' | 'num'} [prop = 'page'] 需要设置的参数'page' | 'size' | 'num'
+   */
+  setPageData (data, prop = 'page', unTriggerLife) {
+    if (this.$module.pagination) {
+      if (prop == 'page') {
+        this.$module.pagination.setPage(data, unTriggerLife)
+      } else if (prop == 'size') {
+        this.$module.pagination.setSizeAndPage(data, unTriggerLife) // { size, page }
+      } else if (prop == 'num') {
+        this.$module.pagination.setTotal(data)
+      }
+    }
+  }
+  /* --- pagination end --- */
+
+  /* --- choice start --- */
+  /**
+   * 根据option, defaultOption自动判断重置与否
+   * @param {object | string} [option] 参数
+   * @param {object | string} [defaultOption] 默认参数
+   */
+  autoChoiceReset(data) {
+    if (this.$module.choice) {
+      this.$module.choice.autoReset(data)
+    }
+  }
+  /**
+   * 数据变更=>id作为唯一基准
+   * @param {string[]} idList ID列表
+   * @param {object[]} currentList ITEM列表
+   * @param {'auto' | 'force'} [check = 'auto'] 检查判断值,auto在长度相等时直接认为格式符合，否则进行格式化判断
+   * @param {string} [idProp] id的属性,不存在时自动从DL中获取
+   */
+  changeChoice(idList, currentList, check, idProp) {
+    if (this.$module.choice) {
+      if (!idProp) {
+        idProp = this.getDictionaryPropData('prop', 'id')
+      }
+      this.$module.choice.changeData(idList, currentList, check, idProp)
+    }
+  }
+  /**
+   * 重置操作
+   * @param {boolean} force 重置判断值
+   */
+  resetChoice(force) {
+    if (this.$module.choice) {
+      this.$module.choice.reset(force)
+    }
+  }
+  /**
+   * 获取选项数据
+   * @param {'id' | 'list'} [prop] 存在prop获取data[prop]，否则获取{id, list}
+   * @returns {string[] | object[] | {id, list}}
+   */
+  getChoiceData (prop) {
+    if (this.$module.choice) {
+      return this.$module.choice.getData(prop)
+    }
+  }
+  /* --- choice end --- */
+
   /* --- load start --- */
   /**
    * 数据相关函数定义
@@ -481,6 +567,22 @@ class BaseData extends DefaultData {
 
   /* --- reset start --- */
 
+  /**
+   * 清空数组
+   * @param {array} list 数组
+   */
+  resetArray(list = []) {
+    _func.clearArray(list)
+  }
+  /**
+   * 清空对象
+   * @param {object} data 对象
+   */
+  resetObject(data = {}) {
+    for (let prop in data) {
+      delete data[prop]
+    }
+  }
   /**
    * 将第一个传参的第一个参数无值时转换为空对象
    * @param {*[]} args 参数列表
