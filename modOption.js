a5f59bbc9a8951175bba48b111cfa30eea304eba
MarAngle <yinweiyingzhe@126.com>
2021-12-09 09:51:19 +0800

    优化modOption

--- modOption.js
+++ modOption.js
@@ -1,75 +1,57 @@
 
+let mapDictionary = new Map()
+
 let maindata = {
-  data: {}
-}
-
-maindata.setData = function (data = {}) {
-  let temp = {}
-  for (let n in data) {
-    if (!data[n].type) {
-      this.data[n] = data[n]
+  setDictionary(name, data, redirect) {
+    if (!redirect) {
+      mapDictionary.set(name, data)
     } else {
-      temp[n] = data[n]
+      let target = mapDictionary.get(data)
+      mapDictionary.set(name, target)
     }
-  }
-  for (let n in temp) {
-    this.data[n] = this.data[temp[n].type]
-  }
-}
-
-maindata.getData = function (prop) {
-  if (this.data[prop]) {
-    return this.data[prop]
-  } else {
-    return false
-  }
-}
-
-maindata.getDataByProp = function (prop, payload) {
-  let modprop = payload.mod || prop
-  return this.getData(modprop)
-}
-
-maindata.build = function (data, prop, payload = {}) {
-  let moditem = this.getDataByProp(prop, payload)
-  if (moditem && moditem.build) {
-    moditem.build(data, prop, payload)
-  }
-}
-
-maindata.format = function (ditem, moddata) {
-  if (!moddata) {
-    moddata = {}
-  }
-  if (moddata.edit) {
-    this.formatNext(ditem, 'edit', moddata.edit)
-    delete moddata.edit
-  }
-  for (let n in moddata) {
-    let moditem = moddata[n]
-    this.formatNext(ditem, n, moditem)
-  }
-}
-
-maindata.formatNext = function (ditem, prop, moditem) {
-  if (!moditem.formatType) {
-    moditem.formatType = prop
-  }
-  ditem.setInterface('modType', prop, moditem.formatType)
-  let formatItem = this.getData(moditem.formatType)
-  if (formatItem && formatItem.format) {
-    formatItem.format(ditem, prop, moditem)
-  } else {
-    ditem.$mod[prop] = moditem
-  }
-}
-
-maindata.unformat = function (ditem, prop, payload) {
-  let formatItem = this.getDataByProp(prop, payload)
-  if (formatItem && formatItem.unformat) {
-    return formatItem.unformat(ditem, prop, payload)
-  } else {
-    return ditem.$mod[prop]
+  },
+  getDictionary(name) {
+    return mapDictionary.get(name)
+  },
+  getDictionaryByMod(name, payload) {
+    return this.getDictionary(payload.mod || name)
+  },
+  format(ditem, modData) {
+    if (!modData) {
+      modData = {}
+    }
+    let redirect = {}
+    for (const name in modData) {
+      let modItemData = modData[name]
+      if (modItemData.$target) {
+        redirect[name] = modItemData
+      } else {
+        this.formatItem(ditem, name, modItemData)
+      }
+    }
+  },
+  formatItem(ditem, name, modItemData) {
+    if (!modItemData.$type) {
+      modItemData.$type = name
+    }
+    ditem.setInterface('modType', name, modItemData.$type)
+    let formatDictionary = mapDictionary.getDictionary(modItemData.$type)
+    if (formatDictionary && formatDictionary.format) {
+      ditem.$mod[name] = formatDictionary.format(ditem, name, modItemData)
+    } else {
+      ditem.$mod[name] = modItemData
+    }
+  },
+  build() {
+    alert('未定义！！！')
+  },
+  unformat(ditem, name, payload) {
+    let formatDictionary = this.getDictionaryByMod(name, payload)
+    if (formatDictionary && formatDictionary.unformat) {
+      return formatDictionary.unformat(ditem, name, payload)
+    } else {
+      return ditem.$mod[name]
+    }
   }
 }
 
