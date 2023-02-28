55d8ab710e1ec0d5452d45e00975384f0fb2c32f
Shen GuoQing <yinweiyingzhe@126.com>
2023-02-28 14:57:18 +0800

    准备开始新版本

--- index.ts
+++ index.ts
@@ -1,53 +1,6 @@
 import Data from './src/data/Data'
-import SimpleData from './src/data/SimpleData'
-import DefaultData from './src/data/DefaultData'
-import BaseData from './src/data/BaseData'
 
-import EmptyData from './src/lib/EmptyData'
-import IdData from './src/lib/IdData'
-import InterfaceData from './src/lib/InterfaceData'
-import LifeItem from './src/lib/LifeItem'
-import LifeData from './src/lib/LifeData'
-import ModuleData from './src/lib/ModuleData'
-import StatusItem from './src/lib/StatusItem'
-import StatusData from './src/lib/StatusData'
-import PromiseData from './src/lib/PromiseData'
-import OptionData from './src/lib/OptionData'
-import PaginationData from './src/lib/PaginationData'
-import ChoiceData from './src/lib/ChoiceData'
-import UpdateData from './src/lib/UpdateData'
-import LayoutData from './src/lib/LayoutData'
-import DictionaryItem from './src/lib/DictionaryItem'
-import DictionaryList from './src/lib/DictionaryList'
-import PageList from './src/lib/PageList'
-import DefaultEdit from './src/lib/DefaultEdit'
-
-import dictionaryFormatOption from './dictionaryFormatOption'
 
 export {
   Data,
-  SimpleData,
-  DefaultData,
-  BaseData,
-
-  EmptyData,
-  IdData,
-  InterfaceData,
-  LifeItem,
-  LifeData,
-  ModuleData,
-  StatusItem,
-  StatusData,
-  PromiseData,
-  OptionData,
-  PaginationData,
-  ChoiceData,
-  UpdateData,
-  LayoutData,
-  DictionaryItem,
-  DictionaryList,
-  PageList,
-  DefaultEdit,
-
-  dictionaryFormatOption
 }
