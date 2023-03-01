c020c3d968119754cf83429107b0d3336000a265
Shen GuoQing <yinweiyingzhe@126.com>
2023-03-01 11:37:00 +0800

    优化status

--- src/lib/StatusData.ts
+++ src/lib/StatusData.ts
@@ -1,7 +1,99 @@
-import Data from "../data/Data";
+import config from '../../config'
+import BaseData from '../data/BaseData'
+import { formatInitOption } from '../utils'
+import Data from './../data/Data'
+import StatusItem, { StatusItemInitOption, itemType, valueType } from './StatusItem'
+
+type StatusDataInitOptionItem = {
+  prop: string,
+  data: StatusItemInitOption
+}
+
+export type StatusDataInitOption = {
+  list?: StatusDataInitOptionItem[]
+}
 
 class StatusData extends Data {
   static $name = 'StatusData'
+  data: {
+    [prop: string]: StatusItem
+  }
+  constructor (initOption?: StatusDataInitOption) {
+    initOption = formatInitOption(initOption)
+    super()
+    this.data = {}
+    const dataList = (config.StatusData.list as StatusDataInitOptionItem[]).concat(initOption!.list || [])
+    dataList.forEach(item => {
+      this.data[item.prop] = new StatusItem(item.data)
+    })
+  }
+  addData(target: string, data: StatusItemInitOption, replace?: boolean) {
+    if (!this.data[target] || replace) {
+      // 当不存在对应数据或者需要置换时进行添加操作
+      this.data[target] = new StatusItem(data)
+    }
+  }
+  deleteData(target: string, reset?: boolean) {
+    if (this.data[target]) {
+      if (reset !== false) {
+        this.data[target].reset()
+      }
+      delete this.data[target]
+    }
+  }
+  /**
+   * 获取指定status的prop属性
+   * @param {string} target 指定status
+   * @param {string} [prop] 获取整个或者属性值
+   * @returns {*}
+   */
+  getData(target: string): itemType
+  getData(target: string, prop: valueType): valueType
+  getData (target: string, prop?: valueType) {
+    if (prop) {
+      return this.data[target].getData(prop)
+    } else {
+      return this.data[target].getData()
+    }
+  }
+  /**
+   * 设置指定status的值
+   * @param {string} target 指定status
+   * @param {string} data 指定的属性值
+   * @param {'init' | 'reset'} [act] 操作判断值
+   */
+  setData (target: string, data: valueType, act?: 'init' | 'reset') {
+    this.data[target].setData(data, act)
+  }
+  /**
+   * 重置
+   */
+  reset () {
+    for (const n in this.data) {
+      this.data[n].reset()
+    }
+  }
+  /**
+   * 模块加载
+   * @param {object} target 加载到的目标
+   */
+  $install (target: BaseData) {
+    target.$onLife('beforeReset', {
+      id: this.$getId('BeforeReset'),
+      data: (instantiater, resetOption) => {
+        if (target.$parseResetOption(resetOption, 'status') !== false) {
+          this.reset()
+        }
+      }
+    })
+  }
+  /**
+   * 模块卸载
+   * @param {object} target 卸载到的目标
+   */
+  $uninstall(target: BaseData) {
+    target.$offLife('beforeReset', this.$getId('BeforeReset'))
+  }
 }
 
 export default StatusData
---
+++ src/lib/StatusItem.ts
@@ -0,0 +1,148 @@
+import Data from "../data/Data"
+
+export type valueType = string
+
+export interface itemType {
+  value: valueType,
+  label: valueType,
+  [prop: string]: valueType
+}
+
+type defaultOption = {
+  type: 'default'
+}
+
+type countOption = {
+  type: 'count',
+  prop: string,
+  num: number
+}
+
+type countOptionOption = {
+  type: 'count',
+  prop: string
+}
+
+type StatusItemInitOptionOption = Partial<defaultOption> | countOptionOption
+
+export type StatusItemInitOption = {
+  list: itemType[],
+  current?: valueType,
+  default?: valueType,
+  option?: StatusItemInitOptionOption
+}
+class StatusItem extends Data {
+  static $name = 'StatusItem'
+  option: defaultOption | countOption
+  list: {
+    [prop: string]: itemType
+  }
+  current: itemType
+  default: string
+  constructor (initOption: StatusItemInitOption) {
+    if (!initOption.list || initOption.list.length == 0) {
+      console.error(`StatusItem未设置初始化列表`)
+    }
+    super()
+    if (!initOption.option) {
+      initOption.option = {}
+    }
+    if (initOption.option.type == 'count') {
+      this.option = {
+        type: initOption.option.type,
+        prop: initOption.option.prop,
+        num: 0
+      }
+      if (!this.option.prop) {
+        this.$exportMsg(`StatusItem设置count类型需要传递taget目标值!`)
+      }
+    } else {
+      this.option = {
+        type: 'default'
+      }
+    }
+    this.list = {}
+    this.current = {
+      value: '',
+      label: ''
+    }
+    for (const n in initOption.list) {
+      this.list[initOption.list[n].value] = initOption.list[n]
+    }
+    const current = initOption.current || initOption.list[0].value
+    this.setData(current, 'init')
+    this.default = initOption.default || current // value值
+  }
+  /**
+   * 设置当前值
+   * @param {string} prop 指定的属性值
+   * @param {'init' | 'reset'} [act] 操作判断值
+   */
+  setData (prop: string, act?: 'init' | 'reset') {
+    if (this.list[prop]) {
+      let build = true
+      if (!act) {
+        build = this.$triggerTarget(prop)
+      } else if (act == 'init') {
+        //
+      } else if (act == 'reset') {
+        this.$resetTarget()
+      }
+      if (build && this.current.value != this.list[prop].value) {
+        this.current.value = this.list[prop].value
+        this.current.label = this.list[prop].label
+      }
+    } else {
+      this.$exportMsg(`当前加载判断值${prop}不存在`)
+    }
+  }
+  /**
+   * 重置计算值
+   */
+  $resetTarget () {
+    if (this.option.type == 'count') {
+      this.option.num = 0
+    }
+  }
+  /**
+   * 判断是否需要计算以及下一步操作
+   * @param {string} prop 属性值
+   * @returns {boolean}
+   */
+  $triggerTarget (prop: string) {
+    let fg = true
+    if (this.option.type == 'count') {
+      if (this.option.prop == prop) {
+        this.option.num++
+      } else {
+        this.option.num--
+        if (this.option.num != 0) {
+          fg = false
+        }
+      }
+    }
+    return fg
+  }
+  /**
+   * 获取值
+   * @param {string} [prop] 整个或者属性值
+   * @returns {*}
+   */
+  getData(): itemType
+  getData(prop: valueType): valueType
+  getData (prop?: valueType): itemType | valueType {
+    if (prop) {
+      return this.current[prop]
+    } else {
+      return this.current
+    }
+  }
+  /**
+   * 重置
+   */
+  reset () {
+    this.setData(this.default, 'reset')
+  }
+}
+
+export default StatusItem
