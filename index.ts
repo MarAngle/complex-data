5957310695a0d927644913a35d482b1aa1ef26c3
Shen GuoQing <yinweiyingzhe@126.com>
2023-10-31 14:24:13 +0800

    优化输出

--- index.ts
+++ index.ts
@@ -1,7 +1,28 @@
-
 import Data from './src/data/Data'
+import SimpleData from './src/data/SimpleData'
+import DefaultData from './src/data/DefaultData'
+import BaseData from './src/data/BaseData'
+
+import AttributeData from './src/lib/AttributeData'
+import StatusData from './src/lib/StatusData'
+import PromiseData from './src/lib/PromiseData'
+import ModuleData from './src/lib/ModuleData'
+import DependData from './src/lib/DependData'
+import UpdateData from './src/lib/UpdateData'
+import PaginationData from './src/lib/PaginationData'
 
 export {
 
-  Data
+  Data,
+  SimpleData,
+  DefaultData,
+  BaseData,
+  
+  AttributeData,
+  StatusData,
+  PromiseData,
+  ModuleData,
+  DependData,
+  UpdateData,
+  PaginationData,
 }
