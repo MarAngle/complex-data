11f924d3092d33582d3dfd1b9ae4dbce09d9a02e
Shen GuoQing <yinweiyingzhe@126.com>
2024-01-09 08:42:46 +0800

    4.1.7

--- src/dictionary/DefaultEditFile.ts
+++ src/dictionary/DefaultEditFile.ts
@@ -9,15 +9,20 @@ export interface uploadFileDataType {
 }
 
 export interface DefaultEditFileOption {
-  type?: string
-  icon?: DefaultEditButtonOption['icon']
   accept?: string
-  multipleAppend: boolean
-  max: number
-  min: number
-  size: number
-  upload?: ((file: File) => Promise<{ file: uploadFileDataType }>) | ((file: File[]) => Promise<{ file: uploadFileDataType[] }>)
-  layout: string
+  size?: number
+  upload?: (file: File | File[]) => Promise<{ file: uploadFileDataType | uploadFileDataType[] }>
+  layout?: string
+  multiple?: {
+    min?: number
+    max?: number
+    append?: boolean
+  }
+  button?: {
+    name?: DefaultEditButtonOption['name']
+    type?: DefaultEditButtonOption['type']
+    icon?: DefaultEditButtonOption['icon']
+  }
 }
 
 export interface DefaultEditFileInitOption extends DefaultEditInitOption {
@@ -27,28 +32,13 @@ export interface DefaultEditFileInitOption extends DefaultEditInitOption {
 
 class DefaultEditFile extends DefaultEdit{
   static $name = 'DefaultEditFile'
-  static $defaultOption = {
-    multipleAppend: false,
-    layout: 'auto'
-  }
   type: 'file'
   $option: DefaultEditFileOption
   constructor(initOption: DefaultEditFileInitOption, parent?: DictionaryValue, modName?: string) {
     super(initOption, parent, modName)
     this.type = initOption.type
     const option = initOption.option || {}
-    const $defaultOption = (this.constructor as typeof DefaultEditFile).$defaultOption
-    this.$option = {
-      type: option.type,
-      icon: option.icon,
-      accept: option.accept,
-      multipleAppend: option.multipleAppend === undefined ? $defaultOption.multipleAppend : option.multipleAppend,
-      max: option.max || 0,
-      min: option.min || 0,
-      size: option.size || 0,
-      upload: option.upload,
-      layout: option.layout === undefined ? $defaultOption.layout : option.layout,
-    }
+    this.$option = option
   }
 }
 
